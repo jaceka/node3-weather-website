a757b2d6c58d7dd2d0666a70ef9f1570178c7c7f
Adamowicz <jacek.adamowicz@teradata.com>
2020-01-23 10:15:49 +0100

    Added more info to forecast. Corrected precipitation probability.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,9 +8,14 @@ const forecast = ( latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location',undefined)
         } else {
-            callback(undefined,body.daily.data[0].summary + '. It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + (body.currently.precipProbability * 100.00) + '% chance of rain. ' +
+                              'Daily temperature high is: ' + body.daily.data[0].temperatureHigh + ', low is: ' + body.daily.data[0].temperatureLow + 
+                              '. The pressure is ' + body.currently.pressure + '. ')
         }
     })
 }
 
 module.exports = forecast
+
+
+/* add: temp high/low for the day */
